a22e80f230a6c5227bb329cc4015beabc6b3b9bf
creatorjv <jvdevelops@gmail.com>
2023-12-26 13:49:12 -0500

    Completed task add with some styling

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TaskItem from "./components/TaskItem";
 import TaskInput from "./components/TaskInput";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const [taskList, setTaskList] = useState([]);
@@ -25,47 +26,50 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      {!isModalOpen && (
-        <View style={styles.buttonContainer}>
-          <Pressable onPress={() => onShowModalHandler()}>
-            <Text style={styles.modalButton}>+</Text>
-          </Pressable>
-        </View>
-      )}
-      <TaskInput
-        onAddTask={addTaskHandler}
-        onClose={onShowModalHandler}
-        isVisible={isModalOpen}
-      />
-      <View style={styles.taskContainer}>
-        <Text style={styles.taskListTitle} add={addTaskHandler}>
-          My Tasks
-        </Text>
-        <FlatList
-          data={taskList}
-          renderItem={(taskData) => {
-            return (
-              <TaskItem
-                text={taskData.item}
-                onPressTask={onDeleteHandler}
-                id={taskData.item.id}
-              />
-            );
-          }}
-          keyExtractor={(item) => {
-            return item.id;
-          }}
+    <>
+      <StatusBar style="light"/>
+      <View style={styles.appContainer}>
+        {!isModalOpen && (
+          <View style={styles.buttonContainer}>
+            <Pressable onPress={() => onShowModalHandler()}>
+              <Text style={styles.modalButton}>Add A New Task</Text>
+            </Pressable>
+          </View>
+        )}
+        <TaskInput
+          onAddTask={addTaskHandler}
+          onClose={onShowModalHandler}
+          isVisible={isModalOpen}
         />
+        <View style={styles.taskContainer}>
+          <Text style={styles.taskListTitle} add={addTaskHandler}>
+            My Tasks
+          </Text>
+          <FlatList
+            data={taskList}
+            renderItem={(taskData) => {
+              return (
+                <TaskItem
+                  text={taskData.item}
+                  onPressTask={onDeleteHandler}
+                  id={taskData.item.id}
+                />
+              );
+            }}
+            keyExtractor={(item) => {
+              return item.id;
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    paddingTop: 50,
+    paddingTop: 80,
     paddingHorizontal: 16,
   },
   taskContainer: {
@@ -75,18 +79,19 @@ const styles = StyleSheet.create({
   taskListTitle: {
     textAlign: "center",
     fontSize: 24,
+    color: "#b4a0e5"
   },
   buttonContainer: {
-    height: 40,
-    width: 40,
-    borderRadius: 50,
+    borderRadius: 6,
     backgroundColor: "#B4A0E5",
     textAlign: "center",
     justifyContent: "center",
-    alignItems: "center"
+    alignContent: "center",
   },
   modalButton: {
-    fontSize: 32,
+    fontSize: 24,
     textAlign: "center",
+    padding: 0,
+    margin: 0,
   },
 });
--- components/TaskInput.js
+++ components/TaskInput.js
@@ -1,4 +1,11 @@
-import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from "react-native";
 import { useState } from "react";
 
 export default function TaskInput(props) {
@@ -11,21 +18,30 @@ export default function TaskInput(props) {
   return (
     <Modal visible={props.isVisible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.userInput}
           placeholder="New Task"
           onChangeText={taskInputHandler}
           value={enteredTask}
         />
-        <View>
-          <Button
-            title="Add Task"
-            onPress={() => {
-              props.onAddTask(enteredTask);
-              setEnteredTask("");
-            }}
-          />
-          <Button title="Cancel" onPress={props.onClose} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title="Add Task"
+              onPress={() => {
+                props.onAddTask(enteredTask);
+                setEnteredTask("");
+              }}
+              color="#b180f0"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={props.onClose} color="#f31282" />
+          </View>
         </View>
       </View>
     </Modal>
@@ -35,15 +51,30 @@ export default function TaskInput(props) {
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    paddingVertical: 24,
-    flexDirection: "row",
+    padding: 16,
+    backgroundColor: "#311b6b",
     justifyContent: "center",
     alignItems: "center",
   },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
   userInput: {
+    color: "#120438",
     borderWidth: 1,
-    borderColor: "#cccc",
-    width: "70%",
-    padding: 8,
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    borderRadius: 6,
+    width: "100%",
+    padding: 16,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    marginHorizontal: 8,
   },
 });
